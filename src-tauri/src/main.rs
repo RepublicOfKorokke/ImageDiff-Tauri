228da028d12b4cb60a22d2339e78e911c70c6e07
RepublicOfKorokke <republicofkorokke@users.noreply.github.com>
2023-06-11 08:25:23 +0900

    Add basic ImageDiff

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,18 +3,13 @@
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, print_log])
+        .invoke_handler(tauri::generate_handler![print_log])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
-}
-
-#[tauri::command]
 fn print_log(text: &str) {
-   println!("{}", text)
+    println!("{}", text)
 }
--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,117 @@
-import { invoke } from "@tauri-apps/api/tauri";
-import { ask, open } from '@tauri-apps/api/dialog';
+import { invoke, convertFileSrc } from "@tauri-apps/api/tauri";
+import { open } from "@tauri-apps/api/dialog";
+import { appWindow, LogicalSize } from "@tauri-apps/api/window";
+// @ts-ignore
+import ImageCompare from "image-compare-viewer";
 
-let ogaMsg = document.querySelector("#oga-msg");
+const leftSideImage = document.getElementById(
+  "leftSideImage"
+) as HTMLImageElement;
 
-window.addEventListener("DOMContentLoaded", () => {
-  document
-    .querySelector("#oga-button")
-    ?.addEventListener("click", () => showDialog("Title", "msg"));
-  document
-    .querySelector("#open-button")
-    ?.addEventListener("click", () => showOpenDialog());
+const leftSideButton = document.getElementById(
+  "buttonOpenLeftSideImage"
+) as HTMLImageElement;
+
+const rightSideImage = document.getElementById(
+  "rightSideImage"
+) as HTMLImageElement;
+
+const rightSideButton = document.getElementById(
+  "buttonOpenRightSideImage"
+) as HTMLImageElement;
+
+document.addEventListener("DOMContentLoaded", () => {
+  leftSideButton?.addEventListener("click", () =>
+    selectImage((path) => {
+      setImage(path, true);
+    })
+  );
+  rightSideButton?.addEventListener("click", () =>
+    selectImage((path) => {
+      setImage(path, false);
+    })
+  );
 });
 
-async function showDialog(title: string, msg: string): Promise<Boolean> {
-    const yes = await ask(msg, title);
-    if (yes) {
-        invoke("print_log", {text: "yes"});
-    } else {
-        invoke("print_log", {text: "no"});
-    }
-    return yes
-}
-
-async function showOpenDialog() {
-    const selected = await open({
-      multiple: false,
-      filters: [{
-        name: 'Image',
-        extensions: ['png', 'jpg']
-      }]
-    });
-    if (Array.isArray(selected)) {
-        invoke("print_log", {text: `Multiple items ${selected}`}); // only invoked on multiple flag set false
-    } else if (selected === null) {
-        invoke("print_log", {text: "Canceled"});
-    } else {
-        invoke("print_log", {text: `Single item ${selected}`}); // only invoked on multiple flag set false
-    }
-}
-
-window.addEventListener('keydown', function(event) {
-   if (ogaMsg != null) {
-       ogaMsg.textContent = event.key;
-   }
+window.addEventListener("load", () => {
+  compareImage();
 });
+
+async function selectImage(onSelected: (path: string) => void) {
+  const path = await open({
+    multiple: false,
+    filters: [
+      {
+        name: "Image",
+        extensions: ["png", "jpg", "jpeg", "bmp", "webp"],
+      },
+    ],
+  });
+  if (path) {
+    invoke("print_log", { text: `Selected image: ${path}` });
+
+    let element = new Image();
+    element.onload = function () {
+      var width = element.naturalWidth;
+      var height = element.naturalHeight;
+      changeWindowSize(width, height);
+    };
+    element.src = convertFileSrc(path as string);
+    onSelected(path as string);
+  } else {
+    invoke("print_log", { text: "Canceled" });
+  }
+}
+
+async function changeWindowSize(width: number, height: number) {
+  width = width > 1280 ? 1280 : width;
+  height = height > 720 ? 720 : height;
+  appWindow.setSize(new LogicalSize(width, height));
+}
+
+function setImage(path: string, isLeftSide: boolean) {
+  let pathSrc = convertFileSrc(path as string);
+  let targetImage = isLeftSide ? leftSideImage : rightSideImage;
+  let targetButton = isLeftSide ? leftSideButton : rightSideButton;
+
+  if (targetImage) {
+    targetImage.src = pathSrc;
+  }
+  if (targetButton) {
+    targetButton.textContent = path.substring(path.lastIndexOf("/") + 1);
+  }
+}
+
+function compareImage() {
+  const element = document.getElementById("image-compare");
+
+  const options = {
+    controlColor: "#FFFFFF",
+    controlShadow: true,
+    addCircle: false,
+    addCircleBlur: true,
+
+    // Label Defaults
+
+    showLabels: false,
+    labelOptions: {
+      before: "Before",
+      after: "After",
+      onHover: false,
+    },
+
+    // Smoothing
+
+    smoothing: false,
+    smoothingAmount: 100,
+
+    // Other options
+
+    hoverStart: true,
+    verticalMode: false,
+    startingPoint: 50,
+    fluidMode: false,
+  };
+
+  new ImageCompare(element, options).mount();
+}
